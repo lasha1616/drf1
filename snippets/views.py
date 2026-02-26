8548196a317c77d0e2e74104d36e2e9d8695f66b
lasha1616 <lasha1616@yahoo.com>
2026-02-26 20:27:54 +0400

    associating snippets with logged in users

--- snippets/views.py
+++ snippets/views.py
@@ -15,6 +15,9 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perfform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class UserList(generics.RetrieveAPIView):
     queryset = User.objects.all()
